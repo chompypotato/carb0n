57af765d5bade3dfb526260b9f75fbda61bf3126
DJXCLAW <83291720+DJXCLAW@users.noreply.github.com>
2025-04-23 10:29:14 -0400

    Create panickey.js

---
+++ js/panickey.js
@@ -0,0 +1,9 @@
+document.addEventListener("keydown", function(event) {
+  if (event.key === "`") {
+    openSite(); 
+  }
+});
+
+openSite(){
+window.location.href = "https://www.drive.google.com/";
+}
